a1bf940aaac3f9f735cbe5507b81c7a1f81de732
Alexander Gruver <agruver@g.hmc.edu>
2015-03-07 17:47:59 -0800

    redid data for rl8p8t8

--- ContractFieldFieldScalar/CFFS.cpp
+++ ContractFieldFieldScalar/CFFS.cpp
@@ -2165,7 +2165,7 @@ int main(int argc, char* argv[]) {
   // ********************** < input> ******************************
   // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
   const vector<unsigned int> contractionSizes =
-    {{8, 27 /*, 16, 32, 64, 128, 512, 1024, 2048*/}};
+    {{8, /*27 , 16, 32, 64, 128, 512, 1024, 2048*/}};
   const array<float, 2> memorySizeExtrema = {{1e6, 1e9}};
   const unsigned int numberOfMemorySizes = 5;
   const unsigned int maxNumberOfCudaBlocks = unsigned(1e4);
