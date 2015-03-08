b56f1df33cebb1cac23c400f8743d5e8746e4be0
agruver04 <agruver@g.hmc.edu>
2015-03-07 18:16:32 -0800

    Update plot2d.py

---
+++ ContractFieldFieldScalar/plot2d.py
@@ -0,0 +1,106 @@
+import numpy as np
+import os
+import sys
+
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import csv
+
+
+
+prefix = 'data/ContractFieldFieldScalar'
+outputPrefix = 'figures/'
+suffix = '_clearCache_shadowfax'
+
+numberOfCells = np.loadtxt(open(prefix + 'numberOfContractions' + suffix + '.csv','rb'),delimiter=',',skiprows=0)
+dotProductSize = np.loadtxt(open(prefix + 'contractionSize' + suffix + '.csv','rb'),delimiter=',',skiprows=0)
+
+serialTimes = np.loadtxt(open(prefix + 'serialTimes' + suffix + '.csv','rb'),delimiter=',',skiprows=0)
+kokkosOmpTimes = np.loadtxt(open(prefix + 'kokkosOmpTimes' + suffix + '.csv','rb'),delimiter=',',skiprows=0)
+kokkosCudaIndependentTimes = np.loadtxt(open(prefix + 'kokkosCudaIndependentTimes' + suffix + '.csv','rb'),delimiter=',',skiprows=0)
+kokkosCudaTeamReductionTimes = np.loadtxt(open(prefix + 'kokkosTeamReductionTimes' + suffix + '.csv','rb'),delimiter=',',skiprows=0)
+kokkosCudaTiling = np.loadtxt(open(prefix + 'kokkosCudaTilingTimes' + suffix + '.csv','rb'),delimiter=',',skiprows=0)
+
+
+
+allTimes = [serialTimes, kokkosOmpTimes, kokkosCudaIndependentTimes,
+            kokkosCudaTeamReductionTimes, kokkosCudaTiling]
+
+labels = ["Serial", "Kokkos Omp", "Kokkos Cuda Flat Parallel", 
+          "Kokkos Cuda Team Reduction", "Kokkos Cuda Tiling"]
+
+colors = cm.gist_ncar(np.linspace(1, 0, len(allTimes)))
+
+fig3d = plt.figure(0)
+fig2d = plt.figure(1, figsize=(14, 6))
+ax2d = plt.subplot(111)
+box2d = ax2d.get_position()
+ax2d.set_position([box2d.x0, box2d.y0, box2d.width * 0.60, box2d.height])
+bbox_to_anchor2d = (1.87, 0.5)
+
+#log plots
+for useCase in xrange(len(numberOfCells)):
+    plt.cla()
+    plt.plot(numberOfCells[useCase], serialTimes[useCase],
+             numberOfCells[useCase], kokkosOmpTimes[useCase],
+             numberOfCells[useCase], kokkosCudaIndependentTimes[useCase],
+             numberOfCells[useCase], kokkosCudaTeamReductionTimes[useCase],
+             numberOfCells[useCase], kokkosCudaTiling[useCase])
+
+
+    plt.xscale('log')
+    plt.yscale('log')
+    plt.title('raw times for use case number %d' % (useCase + 1), fontsize=16)
+    plt.xlabel('number of cells', fontsize=16)
+    plt.ylabel('raw time [seconds]', fontsize=16)
+    plt.xlim(numberOfCells[useCase][0], numberOfCells[useCase][-1])
+
+    ax2d.legend(labels, loc='center right', bbox_to_anchor=bbox_to_anchor2d)
+
+    filename = outputPrefix + "FixedSize_RawTimes_2d_UseCase" + str(useCase + 1) + suffix
+    plt.savefig(filename + ".pdf")
+
+    print "saved file %s" % filename
+
+
+#linear plots
+for useCase in xrange(len(numberOfCells)):
+    plt.cla()
+    plt.plot(numberOfCells[useCase], serialTimes[useCase],
+             numberOfCells[useCase], kokkosOmpTimes[useCase],
+             numberOfCells[useCase], kokkosCudaIndependentTimes[useCase],
+             numberOfCells[useCase], kokkosCudaTeamReductionTimes[useCase],
+             numberOfCells[useCase], kokkosCudaTiling[useCase])
+
+
+    plt.xscale('linear')
+    plt.yscale('linear')
+    plt.title('raw times for use case number %d' % (useCase + 1), fontsize=16)
+    plt.xlabel('number of cells', fontsize=16)
+    plt.ylabel('raw time [seconds]', fontsize=16)
+    plt.xlim(numberOfCells[useCase][0], numberOfCells[useCase][-1])
+
+    ax2d.legend(labels, loc='center right', bbox_to_anchor=bbox_to_anchor2d)
+
+    filename = outputPrefix + "FixedSize_RawTimes_Linear2d_UseCase" + str(useCase + 1) + suffix
+    plt.savefig(filename + ".pdf")
+
+    print "saved file %s" % filename
+
+
+
+for useCase in xrange(len(numberOfCells)):
+    summArray = numberOfCells[useCase]
+    for timesArray in allTimes:
+        summArray = np.vstack((summArray, timesArray[useCase]))
+
+    allLabels = np.asarray(["Number of Cells"] + labels)
+    summArray = np.column_stack([allLabels, summArray])
+
+    filename = "data/FixedSize_UseCase" + str(useCase + 1) + "_summary.csv"
+    with open(filename, "wb") as f:
+            writer = csv.writer(f)
+            writer.writerows(summArray)
+
+    print "saved file %s" % filename
