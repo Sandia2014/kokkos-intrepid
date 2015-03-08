b30f63df0d1f4f12fd2c5b9fb9e2482164711aba
Alexander Gruver <agruver@g.hmc.edu>
2015-03-07 19:21:37 -0800

    new data for rl8p8t8

--- ContractFieldFieldScalar/CFFS.cpp
+++ ContractFieldFieldScalar/CFFS.cpp
@@ -2156,11 +2156,11 @@ int main(int argc, char* argv[]) {
   // ********************** < input> ******************************
   // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
   const vector<unsigned int> contractionSizes =
-    {{/*8, 16, 32,*/ 216, /*128, 512, 1024/*, 2048*/}};
+    {{/*8, 16, 32,*/ 8, /*128, 512, 1024/*, 2048*/}};
   const array<float, 2> memorySizeExtrema = {{1e6, 1e9}};
   const unsigned int numberOfMemorySizes = 5;
   const unsigned int maxNumberOfCudaBlocks = unsigned(1e4);
-  const unsigned int tile_size = 16;
+  const unsigned int tile_size = 8;
   const ClearCacheStyle clearCacheStyle =
     ClearCacheAfterEveryRepeat;
   const unsigned int numberOfRepeats =
@@ -2296,7 +2296,7 @@ int main(int argc, char* argv[]) {
     const unsigned int contractionSize = contractionSizes[contractionSizeIndex];
 
     const int numPoints = contractionSize;
-    const int numBasis = 125;
+    const int numBasis = 8;
 
     const timespec thisSizesTic = getTimePoint();
 
